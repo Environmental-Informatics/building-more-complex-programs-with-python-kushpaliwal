7b8e6aa72f3e0e1e8312d51d5394fef6c21e096b
kushpaliwal <60015800+kushpaliwal@users.noreply.github.com>
2020-02-15 14:28:26 -0500

    Update program_6.5.py

--- program_6.5.py
+++ program_6.5.py
@@ -5,13 +5,21 @@ by Kush Paliwal
 Think Python 2e Exercise 5.2
 This program finds the (greatest commom divisor) gcd of two given numbers
 """
-# define gcd function
+# Define gcd function
 def gcd(a,b):
     if b == 0:
-        return a
+        print(a)
     else:
         r = a%b
         return gcd(b,r)
-    
-ans = gcd(10,6)
-print(ans)
+
+# Take input from user    
+a = input('Input a: ')
+b = input('Input b: ')
+
+# Convert str to float
+a = float(a)
+b = float(b)
+
+# Call function
+gcd(a,b)
